b90557faa2b4a68686829237d6b8c7bc0ebc3706
Abhishek Jain <abhishekjainx1235@gmail.com>
2026-07-12 17:53:12 +0530

    protected dashboard route

--- middleware.js
+++ middleware.js
@@ -1,7 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
-const isProtectedRoute = createRouteMatcher(['/forum(.*)'])
+const isProtectedRoute = createRouteMatcher([
+  '/forum(.*)',
+  '/dashboard(.*)'
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth().protect()
